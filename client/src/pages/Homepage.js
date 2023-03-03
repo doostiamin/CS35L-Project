afa0b5fee71c1524739b4bffc1b1d4597791e984
Gabriella Ang <gabriella90631@gmail.com>
2023-03-03 14:32:50 -0800

    Templates for Homepage and Spell pages
    - made templates for the homepage and spell page with all necessary buttons that will link to the correct pages
    - created a text field that will take in the answer for the spelling and currently alerts with either correct or incorrect
    - text field implementation is still in progress (currently only returns correct if the word is equal to 'scrumptious'

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,33 +1,80 @@
 import { Box } from '@mui/material';
 import Button from '@mui/material/Button';
+import { display, maxHeight } from '@mui/system';
 import { Link } from "react-router-dom";
+import Stack from '@mui/material/Stack';
 
-
-const boxDefault = {
-    height: 770,
+const boxSidePanel = {
+    display: "flex",
+    flexDirection: "column",
+    width: 0.25, 
     padding: 2,
-    minWidth: 600,
-    m: 1
+    minWidth: 100,
+    m: 1,
+    alignItems: "center",
+    backgroundColor: 'LightGray',
+}
+
+const boxSpell = {
+    display: "flex",
+    flexDirection: "column",
+    height: 770,
+    width:0.75,
+    padding: 2,
+    m: 1,
+    alignItems: "center",
+    backgroundColor: 'LightYellow'
+}
+function getUserInfo() {
+    //TODO
 }
 
 export default function Homepage() {
     return (
-      <div id="homepage">
-        <div>
-          <Link to="/spell">
-            <Button variant="outlined" sx={{height:40}}>Spell</Button>
-          </Link>
-        </div>
-        <div>
-          <Link to="/leaderboard">
-            <Button variant="outlined" sx={{height:40}}>Leaderboard</Button>
-          </Link>
-        </div>
-        <div>
-          <Link to="/login">
-            <Button variant="outlined" sx={{height:40}}>Logout</Button>
-          </Link>
-        </div>
-      </div>
-      );
+    <div id="homepage">
+        <Stack direction="row">
+            <Box sx={boxSidePanel}>
+                <h2>Name</h2>
+                <h3>Points:1000</h3>
+                <div>
+                    <Link to="/leaderboard">
+                        <Button variant="outlined" sx={{height:40}}>Leaderboard</Button>
+                    </Link>
+                </div>
+                <div>
+                    <Link to="/login">
+                        <Button variant="outlined" color="error" sx={{height:40}}>Logout</Button>
+                    </Link>
+            </div>
+            </Box>
+            <Box sx={boxSpell} alignItems="center" justifyContent="center">
+                <h1>Spell!</h1>
+                <h2>By Difficulty</h2>
+                <div>
+                    <Link to="/spell">
+                        <Button variant="outlined" sx={{height:40}}>Easy</Button>
+                    </Link>        
+                    <Link to="/spell">
+                        <Button variant="outlined" sx={{height:40}}>Medium</Button>
+                    </Link>
+                    <Link to="/spell">
+                        <Button variant="outlined" sx={{height:40}}>Hard</Button>
+                    </Link>
+                </div>
+                <h2>By Topic</h2>
+                <div>
+                    <Link to="/spell">
+                        <Button variant="outlined" sx={{height:40}}>Category 1</Button>
+                        </Link>
+                    <Link to="/spell">
+                        <Button variant="outlined" sx={{height:40}}>Category 2</Button>
+                    </Link>
+                    <Link to="/spell">
+                        <Button variant="outlined" sx={{height:40}}>Category 3</Button>
+                    </Link>
+                </div>
+            </Box>
+        </Stack>
+    </div>
+    );
 }
--- client/src/pages/Spell.js
+++ client/src/pages/Spell.js
@@ -1,27 +1,54 @@
-import { Box } from '@mui/material';
+import { Box, Icon } from '@mui/material';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { maxHeight } from '@mui/system';
+
 
 const boxDefault = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContents: "center",
     height: 770,
-    padding: 2,
+    padding: 9,
     minWidth: 600,
-    m: 1
+    m: 1,
+    alignItems: "center",
+    gap: 2
 }
 
 export default function Spell() {
-  return (
-    <div id="spell">
-      <div>
-        <Link to="/spell">
-          <Button variant="outlined" sx={{height:40}}>Spell</Button>
-        </Link>
-      </div>
-      <div>
-        <Link to="/homepage">
-          <Button variant="outlined" sx={{height:40}}>Back</Button>
-        </Link>
-      </div>
-    </div>
+    const [answer, setAnswer] = useState("");
+    const [updated, setUpdated] = useState('');
+
+    function checkCorrect() {
+        if (answer === "scrumptious") {
+            alert("Correct");
+        }
+        else {
+            alert("Incorrect");
+        }
+    }
+     
+    return (
+        <div id="spell">
+            <Box sx={boxDefault}>
+                <div>
+                    <TextField id="standard-basic" label="Answer" variant="standard" onChange={(answer) => setAnswer(answer.target.value)}/>
+                    <Button variant="contained" sx={{height:40}} onClick={checkCorrect}>Enter</Button>
+                </div>
+                <div>
+                    <Button variant="outlined" sx={{height:40}}>Listen</Button>
+                    <Button variant="outlined" sx={{height:40}}>Definition</Button>
+                    <Button variant="outlined" sx={{height:40}}>Skip</Button>
+                </div>
+                <div>
+                    <Link to="/homepage">
+                    <Button variant="outlined" sx={{height:40}}>Back</Button>
+                    </Link>
+                </div>
+            </Box>
+        </div>
     );
 }
