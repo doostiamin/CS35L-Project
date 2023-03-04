cb9de286e9d4f3008d195ed46841f878512a1aa8
Katherine Sohn <katherinejsohn@gmail.com>
2023-03-03 16:08:09 -0800

    Setup basic login functionality, no back-end interaction.
    - made login default page when app opens for usability
    - username and password input stored ready to send to API
    - login button navigates to homepage all the time
    - TODO: username/password authentication

--- client/src/App.js
+++ client/src/App.js
@@ -11,14 +11,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return(
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/homepage" element={<Homepage/>}></Route>
-        <Route path="/leaderboard" element={<Leaderboard/>}></Route>
-        <Route path="/spell" element={<Spell/>}></Route>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Login/>}></Route>
+          <Route path="/login" element={<Login/>}></Route>
+          <Route path="/homepage" element={<Homepage/>}></Route>
+          <Route path="/leaderboard" element={<Leaderboard/>}></Route>
+          <Route path="/spell" element={<Spell/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,22 +1,54 @@
-import { Box } from '@mui/material';
+import { Box, FormControl, TextField } from '@mui/material';
 import Button from '@mui/material/Button';
-import { Link } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
+import { useState } from 'react';
 
-const boxDefault = {
-    height: 770,
-    padding: 2,
-    minWidth: 600,
-    m: 1
-}
+const login = {
+  margin: "5px"
+};
 
-export default function Login() {
-    return (
-      <div id="login">
-        <Link to="/homepage">
-          <Box sx={boxDefault} display="flex" justifyContent="center" alignItems="center">
-            <Button variant="outlined" sx={{height:40}}>Login</Button>
-          </Box>
-        </Link>
-      </div>
-      );
-}
+const Login = () => {
+  
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function authenticate(){
+    console.log("authenticating ", username, "\npassword ", password);
+
+    // API call to navigate only if valid authentication
+    if(true){
+      navigate('/homepage');
+    }
+    //else
+     // alert("invalid username and/or password.");
+  }
+
+  return (
+    <Box height="100vh" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+      <Box>
+        <TextField id="email" 
+          type="email" 
+          placeholder="email" 
+          onChange={(e) => setUsername(e.target.value)}
+          sx={login}
+        />
+        <Box display="flex">
+          <TextField id="password" 
+            type="password" 
+            placeholder="password" 
+            onChange={(e) => setPassword(e.target.value)}
+            sx={login} 
+          />
+          <Button variant="outlined" 
+            onClick={() => authenticate()}
+            sx={login}
+          >login</Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Login;
