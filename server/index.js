abb4d8249429e202ffb1a43bb7ce79602fcabf69
Amin Doosti <devboiamin@gmail.com>
2023-03-10 13:07:04 -0800

    Implement GET /api/users to grab and send the full list of users
    Changes in detail:
            - Added /data/users.json file to act as our user database
            - Added getUsers() helper function in index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const fs = require('fs');
 
+const USERS_DATA_FILE_PATH = './data/users.json';
 const PORT = 8080;
 const app = express();
 
@@ -7,6 +9,32 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
+// USER MANAGEMENT API
+app.get('/api/users', (req, res) => {
+  let {error, data: users} = getUsers();
+
+  if(error) res.status(500);
+
+  res.json({
+    success: error,
+    data: users
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
 });
+
+// Helper Functions
+const getUsers = () => {
+  let data = {};
+  let error = false;
+
+  if(fs.existsSync(USERS_DATA_FILE_PATH)){
+    let raw = fs.readFileSync(USERS_DATA_FILE_PATH);
+    data = JSON.parse(raw);
+    error = true;
+  }
+
+  return {error, data}
+}
