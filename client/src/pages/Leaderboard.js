3db70982450042ae07eb9da3b81acf82c46dbe5d
Katherine Sohn <katherinejsohn@gmail.com>
2023-03-13 14:53:51 -0700

    Basic Leaderboard Functionality
    - see all currently registered users' points
    - added route to leaderboard page
    - dynamically displays server data

--- client/src/pages/Leaderboard.js
+++ client/src/pages/Leaderboard.js
@@ -1,20 +1,76 @@
-import { Box } from '@mui/material';
+import { Box, Table, TableContainer, TableHead, TableRow, TableCell, TableBody} from '@mui/material';
 import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
+import axios from 'axios';
+import { useState, setState, useEffect } from 'react'
+
+// React component, row of the leaderboard
+function Row({name, points}){
+
+  return(
+    <TableRow>
+      <TableCell>{name}</TableCell>
+      <TableCell>{points}</TableCell>
+    </TableRow>
+  );
+}
 
 export default function Leaderboard() {
+
+  const [users, setUsers] =  useState(null);
+
+  // only happens once
+  useEffect(() => {
+
+    // set users only once the API call comes back
+    const getData = async () => {
+      const response = await axios.get('http://localhost:8080/api/users');
+
+        setUsers(response.data.data);
+    }
+
+    try{
+      getData();
+    }
+    catch(err){
+      console.log(err);
+    }
+
+  },[]);
+
+  // only happens once state has updated
+  if(users)
+  {
+    var usernames = Object.keys(users);
+
+    // entries: username, name, points
+    var entries = usernames.map(function(u) {return [u, users[u].name, users[u].points]});
+
+    // sort leaderboard
+    entries.sort(function(first, second) {return second[2] - first[2]});
+  }
+  
   return (
-    <div id="leaderboard">
-      <div>
-        <Link to="/leaderboard">
-          <Button variant="outlined" sx={{height:40}}>Leaderboard</Button>
-        </Link>
-      </div>
-      <div>
-        <Link to="/homepage">
-          <Button variant="outlined" sx={{height:40}}>Back</Button>
-        </Link>
-      </div>
-    </div>
+    <Box height="100vh" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+      <Box width = "30%">
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell><h3>username</h3></TableCell>
+                <TableCell><h3>points</h3></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {users && entries.map((e) => <Row key={e[0]} name={e[1]} points={e[2]}/>)}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+      <Link to="/homepage">
+        <Button variant="outlined" sx={{mt: "3vh"}}>Back</Button>
+      </Link>
+    </Box>
     );
+
 }
